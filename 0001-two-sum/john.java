a579d4c5476f303aaa54bc32b32804fe967bce9f
Hejow <gmlwh124@naver.com>
2025-03-16 22:37:57 +0900

    john(250316): two sum

---
+++ 0001-two-sum/john.java
@@ -0,0 +1,15 @@
+class Solution {
+	public int[] twoSum(int[] nums, int target) {
+		var map = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(nums[i])) {
+				return new int[] {map.get(nums[i]), i};
+			}
+
+			map.put(target - nums[i], i);
+		}
+
+		return new int[] {};
+	}
+}
